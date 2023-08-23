186042a3802d7ca06c0c8fca55865f8c63b73327
Andrew Grathwohl <andrew@grathwohl.me>
2023-08-23 18:44:15 -0400

    ogg

--- Package.swift
+++ Package.swift
@@ -17,7 +17,10 @@ let package = Package(
     dependencies: [
         .package(
            url: "https://github.com/AudioKit/AudioKit.git",
-           .upToNextMajor(from: "5.0.0"))
+           .upToNextMajor(from: "5.0.0")),
+        .package(
+            url: "https://github.com/vector-im/swift-ogg.git",
+            .upToNextMajor(from: "0.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
