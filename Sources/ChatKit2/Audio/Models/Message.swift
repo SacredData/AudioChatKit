d51cb8ff46271738f771ede9a11ba9ecf2a7f598
Andrew Grathwohl <andrew@grathwohl.me>
2023-08-24 15:52:02 -0400

    set spoken language of message once transcript is added

--- Sources/ChatKit2/Audio/Models/Message.swift
+++ Sources/ChatKit2/Audio/Models/Message.swift
@@ -12,23 +12,38 @@ final class Message: ObservableObject {
     let author: String
     let date: Date
     let duration: TimeInterval
+    let feedId: String
     let teamName: String
     let uploadId: String
     
-    var transcript: Transcript?
+    var transcript: Transcript? {
+        didSet {
+            self.spokenLanguage = transcript?.languageOption.languageTag
+        }
+    }
     var translations: [Translation] = []
-    init(audioFile: AVAudioFile, author: String="", date: String="", teamName: String="") {
+    var spokenLanguage: String?
+    var usersListened: [String] = []
+    init(audioFile: AVAudioFile, author: String="", date: String="", feedId: String="", teamName: String="") {
         self.audioFile = audioFile
         self.author = author
         self.date = ISO8601DateFormatter().date(from: date) ?? Date()
         self.duration = audioFile.duration
+        self.feedId = feedId
         self.teamName = teamName
         self.uploadId = audioFile.url.lastPathComponent.replacingOccurrences(of: ".caf", with: "")
     }
+    /// Attach the `Transcript` belonging to this message
+    /// This also sets the message's spoken language property
     func attachTranscript(transcript: Transcript) {
         self.transcript = transcript
     }
+    /// Attach a `Translation` of this message's `Transcript`
     func attachTranslations(translations: [Translation]) {
         self.translations.append(contentsOf: translations)
+    }
+    /// Set this when we know that a new user has listened to the message
+    func setUsersListened(accountIds: [String]) {
+        usersListened.append(contentsOf: accountIds)
     }
 }
