77ee7868dcbd452419771f14f5a7c7df8e069396
Andrew Grathwohl <andrew@grathwohl.me>
2023-08-24 12:38:31 -0400

    add funcs to validate audio formats for different use cases

--- Sources/ChatKit2/Audio/Helpers/AudioConfig.swift
+++ Sources/ChatKit2/Audio/Helpers/AudioConfig.swift
@@ -29,4 +29,20 @@ class AudioConfigHelper: ObservableObject {
     private func setupDefaultSession() {
         // TODO
     }
+    
+    func validateAudioFormatForRecording(audioFormat: AVAudioFormat) -> Bool {
+        let isValidSampleRate = audioFormat.sampleRate == AudioFormats.record?.sampleRate
+        let isValidChannelCount = audioFormat.channelCount == AudioFormats.record?.channelCount
+        let isValidBitDepth = audioFormat.commonFormat == AudioFormats.record?.commonFormat
+        let isValidAudioFormat = [isValidSampleRate, isValidChannelCount, isValidBitDepth].allSatisfy({$0 == true})
+        return isValidAudioFormat
+    }
+    
+    func validateAudioFormatForPlayback(audioFormat: AVAudioFormat) -> Bool {
+        let isValidSampleRate = audioFormat.sampleRate == AudioFormats.global?.sampleRate
+        let isValidChannelCount = audioFormat.channelCount == AudioFormats.global?.channelCount
+        let isValidBitDepth = audioFormat.commonFormat == AudioFormats.global?.commonFormat
+        let isValidAudioFormat = [isValidSampleRate, isValidChannelCount, isValidBitDepth].allSatisfy({$0 == true})
+        return isValidAudioFormat
+    }
 }
