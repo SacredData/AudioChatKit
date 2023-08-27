b3248e55f6b83b2ff3d151e8148531c70474baae
agrathwohl <andrew@storyboard.fm>
2023-08-27 12:12:17 -0500

    add a me property to indicate logged-in user

--- Sources/ChatKit2/Audio/Models/Peer.swift
+++ Sources/ChatKit2/Audio/Models/Peer.swift
@@ -9,13 +9,15 @@ import Foundation
 import MultipeerConnectivity
 
 public final class Peer {
+    let me: Bool
     let peerId: MCPeerID
     let name: String
     var teams: [Team]?
-    // TODO: Use a more appropriate type definition
+    // TODO: Use PhoneNumberKit
     let phoneNumber: String?
     let emailAddress: String?
-    public init(id: MCPeerID, name: String, teams: [Team], phoneNumber: String?, emailAddress: String?) {
+    public init(id: MCPeerID, name: String, me: Bool=false, teams: [Team], phoneNumber: String?, emailAddress: String?) {
+        self.me = me // Is this the user logged-in to the app?
         self.peerId = id
         self.name = name
         self.teams = teams
