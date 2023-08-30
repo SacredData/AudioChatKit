1ab819c816704df7270d1828e2996acc3cdaab9a
Andrew Grathwohl <andrew@grathwohl.me>
2023-08-30 13:53:35 -0400

    remove test code from init

--- DemoApp/ChatKitDemo/ChatKitDemo/ChatKitDemoApp.swift
+++ DemoApp/ChatKitDemo/ChatKitDemo/ChatKitDemoApp.swift
@@ -15,34 +15,23 @@ struct ChatKitDemoApp: App {
   @State var configurator: AudioConfigHelper = .init()
   @State var isPlaying: Bool = false
   @State var conductor: AudioConductor = .shared
-
-  var engine: AudioEngine?
-  var player: AudioPlayer?
   var preferredLocalization: String = ""
   
     init() {
         Log(configurator.sessionPreferencesAreValid)
-        preferredLocalization = configurator.preferredLocalization ?? "blah"
         Log(preferredLocalization)
-        
-        engine = conductor.engine
-        Log("ENGINE")
-        Log(engine?.avEngine.isRunning)
-        
-        player = conductor.playerMan.player
-        Log("PLAYER")
         // TODO: Remove this stuff
-        let fileDir = FileManager.default.urls(for: .documentDirectory,
-                                               in: .userDomainMask).first
-        Log(fileDir)
-
-        let filename = fileDir?.appendingPathComponent("SMOLD_020.mp3")
-        Log(filename)
-
-        let file = try! AVAudioFile(forReading: filename!)
-        let message = Message(audioFile: file, author: nil)
-        Log(message.avAsset)
-        try! conductor.playerMan.newLocalMessage(msg: message)
+//        let fileDir = FileManager.default.urls(for: .documentDirectory,
+//                                               in: .userDomainMask).first
+//        Log(fileDir)
+//
+//        let filename = fileDir?.appendingPathComponent("SMOLD_020.mp3")
+//        Log(filename)
+//
+//        let file = try! AVAudioFile(forReading: filename!)
+//        let message = Message(audioFile: file, author: nil)
+//        Log(message.avAsset)
+//        try! conductor.playerMan.newLocalMessage(msg: message)
         // TODO: END
     }
   
